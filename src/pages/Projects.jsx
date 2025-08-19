270582cc883bcf4b541fac72b80669a3a1c8b1f1
Alfiya Simran <alfiyasimran05@gmail.com>
2025-08-19 21:21:48 +0530

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -32,7 +32,12 @@ const projects = [
     desc: "A deep learning-based Brain Tumor Classification system with a Streamlit interface for real-time MRI image analysis",
     techList: ["Python", "TensorFlow/Keras", "OpenCV", "Streamlit", "NumPy", "Matplotlib"],
     repo: "https://github.com/Alfiya-Simran/Brain-Tumor",
-    video: "/videos/braintumor.mp4"
+    images: [
+      "/images/brain1.png",
+      "/images/brain2.png",
+      "/images/brain3.png",
+      "/images/brain4.png"
+    ]
   },
 ];
 
